8634c1032c0d58419807ea3db1bd63ea13357868
Brandon Tyson <betyson@student.fullsail.edu>
2026-03-01 16:59:46 -0500

    Add ESLint + Prettier (JS)

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:react-refresh/recommended",
+      "plugin:prettier/recommended",
+      "prettier"
+    ],
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      ecmaFeatures: { jsx: true }
+    },
+    settings: {
+      react: { version: "detect" }
+    },
+    rules: {
+      "react/prop-types": "warn"
+    }
+  };
